ae2035a15e31618001dac6fa8ef93a8e11d414aa
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-03 13:47:22 +0530

    react persist for avoiding loss of session data in local browser storage

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,28 +1,27 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import { signInStart ,signInSuccess ,signInFailure  } from '../redux/user/userSlice';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading , error} =useSelector((state)=> state.user);
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  const disapatch = useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
-    console.log(formData);
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // setLoading(true);
-      disapatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -33,51 +32,48 @@ const SignIn = () => {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        // setLoading(false);
-        disapatch(signInFailure(data.message));
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      // setLoading(false);
-      disapatch(signInSuccess(data));
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      // setLoading(false);
-      disapatch(signInFailure(error.message));
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
-  
   return (
-    <div className=" mx-auto max-w-lg my-20  border rounded-xl  bg-gray-100 p-4  shadow-lg">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign-in</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
-        
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
-          type="email"
-          placeholder="email"
-          className="border p-3 rounded-lg"
-          id="email"
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
           onChange={handleChange}
         />
         <input
-          type="password"
-          placeholder="password"
-          className="border p-3 rounded-lg"
-          id="password"
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
           onChange={handleChange}
         />
-        <button className="bg-accent text-white p-3 rounded-lg uppercase hover:text-secondary">
-          Sign-In
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
-      <div className="flex gap-2 text-sm mt-3">
-        <span className="text-gray-500">Have an Account?</span>
-        <Link to="/signup" className="text-sm text-blue-500">Sign Up</Link>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
-};
-
-export default SignIn;
+}
