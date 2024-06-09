8b486106cc39ad4f26b0668d58a9e95fa1b20153
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-09 20:27:27 +0530

    show listing api and ui completed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js'
+import Listing from '../models/listing.model.js';
 export const test=(req,res)=>{
     res.json({
         message:"hello ansistors",
@@ -52,3 +53,16 @@ export const deleteUser= async(req,res,next)=>{
         next(error)
     }
 };
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  };
