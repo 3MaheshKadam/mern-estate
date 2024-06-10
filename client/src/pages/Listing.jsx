da6e6347116b37149cc787ed9e75c15e9befeabf
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-10 11:15:08 +0530

    send email functionality to list creators(property owners)

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
+import { useSelector } from 'react-redux';
+import Contact from '../components/Contact';
+
 import 'swiper/css/bundle';
 import {
   FaBath,
@@ -24,6 +27,10 @@ export default function Listing() {
   const [copied, setCopied] = useState(false);
   const params = useParams();
   
+  const [contact, setContact] = useState(false); 
+  const {currentUser} = useSelector((state) => state.user);
+
+
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -134,6 +141,12 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
