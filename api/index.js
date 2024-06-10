291ce6516a35df2503f32b2515b6d3c860fcff13
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-10 22:45:48 +0530

    port not found error to render

--- api/index.js
+++ api/index.js
@@ -25,8 +25,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
   console.log('Server is running on port 3000!');
 });
 
