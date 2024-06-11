c6f005091978a0ecbb1c00dd008a9a1ef7ef0f9f
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-11 10:45:44 +0530

    completed final

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,8 +29,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap text-accent'>
-            <span >Sahand</span>
-            <span >Estate</span>
+            <span >Mahesh</span>
+            <span >Kadam</span>
           </h1>
         </Link>
         <form
